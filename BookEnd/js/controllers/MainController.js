a912ff27e23ecfcc583744825ad5fd301634f5be
Wesley Clark Bastardi <40193725+wesleyclarkbastardi@users.noreply.github.com>
2019-05-10 02:51:43 -0400

    Add files via upload

---
+++ BookEnd/js/controllers/MainController.js
@@ -0,0 +1,44 @@
+app.controller('MainController', [
+    '$scope', function($scope) {
+      $scope.products = [
+        { 
+          name: 'The Book of Trees', 
+          price: 19, 
+          pubdate: new Date('2014', '03', '08'), 
+          cover: 'img/the-book-of-trees.jpg',
+          likes: 0,
+          dislikes: 0
+        }, 
+        { 
+          name: 'Program or be Programmed', 
+          price: 8, 
+          pubdate: new Date('2013', '08', '01'), 
+          cover: 'img/program-or-be-programmed.jpg',
+          likes: 0,
+          dislikes: 0
+        },
+        { 
+          name: 'The Art of Computer Programming I', 
+          price: 29, 
+          pubdate: new Date('2016', '07', '02'), 
+          cover: 'img/art-of-computer-programming.jpg',
+          likes: 0,
+          dislikes: 0
+        },
+        { 
+          name: 'Refactoring', 
+          price: 22, 
+          pubdate: new Date('2019', '01', '01'), 
+          cover: 'img/refactoring.jpg',
+          likes: 0,
+          dislikes: 0
+        } 
+      ];
+      $scope.plusOne = function(index) {
+        $scope.products[index].likes += 1;
+      };
+      $scope.minusOne = function(index) {
+        $scope.products[index].dislikes += 1;
+      };
+    }
+  ]);
